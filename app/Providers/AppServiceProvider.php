a0c804a0b7b76c459fd5fd7d8a388804222ee256
longfei <1371986272@qq.com>
2022-06-16 18:08:51 +0800

    将html文件解析引擎改为blade

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //将html文件解析引擎改为blade
+        View::addExtension('html', 'blade');
     }
 }
