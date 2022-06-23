76eca3a649c1a0192d1269eb990a4e8559f64b52
longfei <1371986272@qq.com>
2022-06-23 11:06:35 +0800

    users新增性别

--- database/migrations/2022_06_21_025045_add_real_name_to_users_table.php
+++ database/migrations/2022_06_21_025045_add_real_name_to_users_table.php
@@ -14,6 +14,7 @@ class AddRealNameToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
+            $table->unsignedTinyInteger('gender')->default(0)->after('remember_token');
             $table->timestamp('birthdate')->nullable()->after('remember_token');
             $table->string('last_name')->default('')->after('remember_token');
             $table->string('first_name')->default('')->after('remember_token');
