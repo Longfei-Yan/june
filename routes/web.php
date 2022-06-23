e9091e294a3820bdaa573a470ff53a39db175e41
longfei <1371986272@qq.com>
2022-06-23 17:56:29 +0800

    所有路由修改为Restful风格

--- routes/web.php
+++ routes/web.php
@@ -20,18 +20,11 @@ Route::group(['namespace' => 'Web', 'middleware'=>'setTheme:WEB_THEME'], functio
     Route::get('/', 'HomeController@index')->name('/');
     Route::get('/home', 'HomeController@index')->name('home');
     Route::get('/index', 'HomeController@index')->name('index');
-
     //认证
     Route::get('/login', 'AuthorizationController@create')->name('auth.create');
     Route::post('/login', 'AuthorizationController@store')->name('auth.store');
     Route::delete('/logout', 'AuthorizationController@destroy')->name('auth.destroy');
-
     //用户
-    Route::get('/register', 'UserController@create')->name('register');
-    Route::post('/register', 'UserController@store')->name('register');
-    Route::get('/my-account', 'UserController@myAccount')->middleware('auth')->name('my-account');
-
-    //用户resource
     Route::get('/users/create', 'UserController@crate')->name('users.create');
     Route::post('/users', 'UserController@store')->name('users.store');
     Route::get('/users', 'UserController@index')->middleware('auth')->name('users.index');
@@ -39,23 +32,23 @@ Route::group(['namespace' => 'Web', 'middleware'=>'setTheme:WEB_THEME'], functio
     Route::get('/users/{user}/edit', 'UserController@edit')->middleware('auth')->name('users.edit');
     Route::patch('/users/{user}', 'UserController@update')->middleware('auth')->name('users.update');
     Route::delete('/users/{user}', 'UserController@destroy')->middleware('auth')->name('users.destroy');
-
     //用户地址
     Route::get('/user-addresses', 'UserAddressController@index')->middleware('auth')->name('user-addresses.index');
-
-
     //商品
-    Route::get('/products', 'ProductController@index')->name('products');
-    Route::get('/products/{id}', 'ProductController@show')->name('product');
-
+    Route::get('/products', 'ProductController@index')->name('products.index');
+    Route::get('/products/{id}', 'ProductController@show')->name('products.show');
     //服务或隐私政策
-    Route::get('/services', 'ServiceController@index')->name('services');
-    Route::get('/about', 'AboutController@index')->name('about');
-    Route::get('/contact', 'ContactController@index')->name('contact');
-    Route::post('/contact', 'ContactController@store')->name('contact');
-    Route::get('/cart', 'CartController@index')->middleware('auth')->name('cart');
-    Route::get('/checkout', 'CheckoutController@index')->middleware('auth')->name('checkout');
-
+    Route::get('/services', 'ServiceController@index')->name('services.index');
+    //关于我们
+    Route::get('/about', 'AboutController@index')->name('about.index');
+    //联系
+    Route::get('/contact', 'ContactController@index')->name('contact.index');
+    Route::post('/contact', 'ContactController@store')->name('contact.store');
+    //购物车
+    Route::get('/cart', 'CartController@index')->middleware('auth')->name('cart.index');
+    //结账
+    Route::get('/checkout', 'CheckoutController@index')->middleware('auth')->name('checkout.index');
+    //404
     Route::fallback(function(){
         return response()->view('404', [], 404);
     });
