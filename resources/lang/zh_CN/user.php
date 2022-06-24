d7709ee6a32412c229a20f3adb53c946947da3bb
longfei <1371986272@qq.com>
2022-06-24 18:17:54 +0800

    管理后台 - 用户列表

---
+++ resources/lang/zh_CN/user.php
@@ -0,0 +1,20 @@
+<?php 
+return [
+    'labels' => [
+        'User' => '用户',
+        'user' => '用户',
+    ],
+    'fields' => [
+        'name' => '名称',
+        'email' => '邮箱',
+        'email_verified_at' => '邮箱验证',
+        'password' => '密码',
+        'remember_token' => '记住我',
+        'first_name' => 'First Name',
+        'last_name' => 'Last Name',
+        'birthdate' => '出生日期',
+        'gender' => '性别',
+    ],
+    'options' => [
+    ],
+];
